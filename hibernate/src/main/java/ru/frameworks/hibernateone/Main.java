e2849f588cf1048ca034dc6eff4779929f40ecac
Artem Luzan <unlimitet2011@yandex.ru>
2019-07-16 20:58:43 +0300

    SpringStartUpdate

--- hibernate/src/main/java/ru/frameworks/hibernateone/Main.java
+++ hibernate/src/main/java/ru/frameworks/hibernateone/Main.java
@@ -11,9 +11,9 @@ public class Main {
 
   public static void main(String[] args) {
     //Author author = new Author("тест");
-    //new AuthorHelper().setAuthor();
+    new AuthorHelper().setAuthor();
 
-    new AuthorHelper().getAuthor(1l);
+    //new AuthorHelper().getAuthor(1l);
 
 //    Session session = HibernateUtil.getSessionfactory().openSession();
 //    session.beginTransaction();
