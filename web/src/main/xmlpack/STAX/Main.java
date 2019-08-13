f2293d812dd347f43b31d39d4ccbab7b54d28abb
Artem Luzan <unlimitet2011@yandex.ru>
2019-08-13 15:59:00 +0300

    X_Path

--- web/src/main/xmlpack/STAX/Main.java
+++ web/src/main/xmlpack/STAX/Main.java
@@ -11,32 +11,24 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, XMLStreamException {
         XMLInputFactory factory = XMLInputFactory.newFactory();
-
         Reader reader = new FileReader("C:\\projects\\frameworks_etc\\web\\src\\main\\xmlpack\\STAX\\books.xml");
-
         XMLStreamReader xmlStreamReader = factory.createXMLStreamReader(reader);
 
         while (xmlStreamReader.hasNext()) {
 
             switch (xmlStreamReader.next()) {
                 case XMLStreamReader.START_ELEMENT :
-
                     String elem = xmlStreamReader.getName().toString();
                     System.out.println("Start element: " + elem);
-
-
                     switch (elem) {
                         case "book" :
-
                             for (int i = 0; i < xmlStreamReader.getAttributeCount(); i++) {
                                 String attributeName = xmlStreamReader.getAttributeLocalName(i);
                                 String attributeValue = xmlStreamReader.getAttributeValue(i);
 
                                 System.out.println(attributeName + " " + attributeValue);
                             }
-
                             break;
-
                         case "title" :
                             System.out.println("Title: " + xmlStreamReader.getElementText());
                             break;
@@ -44,8 +36,6 @@ public class Main {
                             System.out.println("Author: " + xmlStreamReader.getElementText());
                             break;
                     }
-
-
                 case XMLStreamReader.END_ELEMENT :
                     System.out.println("End element: " + xmlStreamReader.getName()+ "\n");
                     break;
