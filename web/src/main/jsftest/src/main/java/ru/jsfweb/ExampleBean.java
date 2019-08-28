6768a8aa6becbe0ddd1fda291727bd34c516b3fc
Artem Luzan <unlimitet2011@yandex.ru>
2019-08-28 19:07:30 +0300

    JSF CDI 2

--- web/src/main/jsftest/src/main/java/ru/jsfweb/ExampleBean.java
+++ web/src/main/jsftest/src/main/java/ru/jsfweb/ExampleBean.java
@@ -1,24 +1,31 @@
 package main.jsftest.src.main.java.ru.jsfweb;
 
+import java.io.Serializable;
 import javax.annotation.PostConstruct;
+import javax.ejb.Lock;
+import javax.ejb.LockType;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 @Named
-public class ExampleBean {
+@SessionScoped
+public class ExampleBean implements Serializable {
 private String name;
 
   @PostConstruct
   private void initialize() {
-    name = "initializate";
+    name = "Hello";
   }
 
   public String getName() {
     return name;
   }
 
+  @Lock(LockType.WRITE)
   public void setName(String name) {
     this.name = name;
   }
+
   public void someAction() {
     name = " ";
   }
