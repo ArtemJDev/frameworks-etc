0e7bc6bafa4d90a78d6e9a1b6f9387dbbab658a7
Artem Luzan <unlimitet2011@yandex.ru>
2019-10-04 11:19:08 +0300

    Http request + parse JSON

--- web/src/main/testeeal/ee/src/http/HttpRequestRead.java
+++ web/src/main/testeeal/ee/src/http/HttpRequestRead.java
@@ -1,10 +1,13 @@
-package main.testeeal.ee.src.http;
+package http;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class HttpRequestRead {
 
@@ -37,12 +40,29 @@ public class HttpRequestRead {
         while((line = reader.readLine()) != null) {
           responceContent.append(line);
         }
-        System.out.println(responceContent.toString());
+//        System.out.println(responceContent.toString());
+
+        parse(responceContent.toString());
       }
     } catch (IOException e) {
       e.printStackTrace();
     } finally {
       connection.disconnect();
     }
+
+
+
+  }
+//add json lib
+  public static String parse(String responseBody) {
+    JSONArray  albums = new JSONArray(responseBody);
+    for (int i = 0; i < albums.length(); i++) {
+      JSONObject album = albums.getJSONObject(i);
+      int id = album.getInt("id");
+      int userId = album.getInt("userId");
+      String title = album.getString("title");
+      System.out.println( id + " " + userId + " " + title);
+    }
+    return null;
   }
 }
