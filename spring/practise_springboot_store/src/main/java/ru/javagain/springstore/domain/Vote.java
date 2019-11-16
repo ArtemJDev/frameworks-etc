1a7cbf418fd3cececfc92d9c878cc4d8784e6d26
Artem Luzan <unlimitet2011@yandex.ru>
2019-11-16 22:54:25 +0300

    [Spring Boot] store + domain(genre,publisher,vote)

---
+++ spring/practise_springboot_store/src/main/java/ru/javagain/springstore/domain/Vote.java
@@ -0,0 +1,34 @@
+package practise_springboot_store.src.main.java.ru.javagain.springstore.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.SelectBeforeUpdate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(catalog = "library")
+@EqualsAndHashCode(of = "id")
+@Getter
+@Setter
+@DynamicUpdate
+@DynamicInsert
+@SelectBeforeUpdate
+public class Vote {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    private String value;
+
+    @Column(name = "book_id")
+    private Date bookId;
+
+    private String username;
+
+}
