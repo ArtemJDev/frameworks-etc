5b6d2f0bdf4367a682c7ae114cec12976742c8d2
Artem <unlimitet2011@yandex.ru>
2019-12-24 12:50:11 +0300

    ClientServer start msg

--- other/clientserver/src/com/clientserver/app/Server.java
+++ other/clientserver/src/com/clientserver/app/Server.java
@@ -8,8 +8,27 @@ public class Server {
     public static void main(String[] args) throws IOException {
 
         ServerSocket serverSocket = new ServerSocket(8000);
-        serverSocket.accept();
-        serverSocket.close();
+        System.out.println("Server running...");
+
+//        clientSocket.getOutputStream().write(64);//send byte to client
+//        clientSocket.getOutputStream().write("<h2>Hello</h2>".getBytes());//send text to client
+
+        int client = 0;
+
+        while(true) {
+            Socket clientSocket = serverSocket.accept();//create clientSocket
+            System.out.println("Client accepted " + client++);
+
+            OutputStreamWriter outputStream = new OutputStreamWriter(clientSocket.getOutputStream());
+            outputStream.write("HTTP/1.0 200 OK\n "
+                + "Content-type: text/html\n"
+                + "\n"
+                + "<h2>Hello</h2>");
+            outputStream.flush();
+            outputStream.close();
+
+            clientSocket.close();
+        }
 
     }
 
