c4e7364fa2806b7e9fbfe1a6127d69935a5f868b
Artem <unlimitet2011@yandex.ru>
2019-12-29 13:41:41 +0300

    ClientServer calculator - phone logic impl

--- other/clientserver/src/com/clientserver/calcapp/Phone.java
+++ other/clientserver/src/com/clientserver/calcapp/Phone.java
@@ -1,28 +1,77 @@
 package com.clientserver.calcapp;
+import java.net.*;
+import java.io.*;
+/**
+ *  universal socket (Client - Server)
+ */
 
 public class Phone {
 
-    public Phone(String port) {
+    ServerSocket server;
+    Socket client;
+    BufferedReader reader;
+    BufferedWriter writer;
 
+    public Phone(String port) {
+        try {
+            server = new ServerSocket(Integer.parseInt(port));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Phone(String ip, String port) {
-
+        try {
+            client = new Socket(ip, Integer.parseInt(port));
+            createStrewams();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void accept() {
+        try {
+            client = server.accept(); // for comm with a client
+            createStrewams();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void createStrewams() {
+        try {
+            reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            writer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public String readLine() {
-        return null;
+        try {
+            return reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void writeLine(String msg) {
-
+        try {
+            writer.write(msg);
+            writer.newLine();
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void close() {
-
+        try {
+            reader.close();
+            writer.close();
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
