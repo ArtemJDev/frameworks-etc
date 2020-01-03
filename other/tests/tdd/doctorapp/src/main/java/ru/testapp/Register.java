004fac537f2e9f7354df5136914f2054c5cf4a20
Artem <unlimitet2011@yandex.ru>
2020-01-03 14:53:28 +0300

    [TDD] practice of use - add logic methods

--- other/tests/tdd/doctorapp/src/main/java/ru/testapp/Register.java
+++ other/tests/tdd/doctorapp/src/main/java/ru/testapp/Register.java
@@ -1,9 +1,25 @@
 package ru.testapp;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Register {
 
-    public Object get(String absent) {
+    Map<String, Object>  register = new HashMap<>();
 
-        return 1;
+    public Object get(String name) {
+        return register.get(name);
+    }
+
+    public void add(String name, Object something) {
+        register.put(name, something);
+    }
+    //add by object name
+    public void add(Object something) {
+        add(something.getClass().getName(), something);
+    }
+
+    public  <T> T get(Class<T> medType) {
+        return (T) get(medType.getName());
     }
 }
--- other/tests/tdd/doctorapp/src/test/java/RegisterTest.java
+++ other/tests/tdd/doctorapp/src/test/java/RegisterTest.java
@@ -1,15 +1,55 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import ru.testapp.Register;
 
 public class RegisterTest {
 
+    private Register register;
+
+
+    @Before
+    public void setUp() throws Exception {
+        register = new Register();
+    }
+
     @Test
     public void absentObjectReturnsNull() {
-        Register register = new Register();
         Object something = register.get("absent");
         Assert.assertNull(something);
-
     }
+
+    //can register new something
+    @Test
+    public void canRegisterSomethingAndRetrieve() {
+        Object one = new Object();
+        register.add("one", one);
+
+        Object two = new Object();
+        register.add("two", two);
+
+        Object retrieveOne = register.get("one");
+        Object retrieveTwo = register.get("two");
+        //test equals also
+        Assert.assertNotNull(retrieveOne);
+        Assert.assertSame(one, retrieveOne);
+
+        Assert.assertNotNull(retrieveTwo);
+        Assert.assertSame(two, retrieveTwo);
+    }
+    //add medications without name
+    @Test
+    public void canRegisterByTypeAndRetrieve() {
+        Pinicilin pinicilin = new Pinicilin();
+        register.add(pinicilin);
+        Purgative purgative = new Purgative();
+        register.add(purgative);
+        Pinicilin retrievePinicilin1 = register.get(Pinicilin.class);
+        Purgative retrievePurgative1 = register.get(Purgative.class);
+
+        Assert.assertSame(pinicilin, retrievePinicilin1);
+        Assert.assertSame(purgative, retrievePurgative1);
+    }
+
 
 }
