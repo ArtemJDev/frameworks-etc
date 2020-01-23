6dd1d4f532cbe7e16306f4e798b7af47d2efc47c
Artem <unlimitet2011@yandex.ru>
2020-01-23 18:13:52 +0300

    [Spring] test Spring Data

---
+++ spring/template/src/main/java/ru/spec/dbspringdata/CourseRepository.java
@@ -0,0 +1,9 @@
+package template.src.main.java.ru.spec.dbspringdata;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Integer> {
+
+    List<Course> findAll();
+}
